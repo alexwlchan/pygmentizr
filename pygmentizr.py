e892a9bc2f555cd4dc1f5677f2c79fa9ac289a7e
Alex Chan <alex@alexwlchan.net>
2015-03-05 22:59:59 +0000

    initial commit
    This is a skeleton app, with a minimal HTML layout to show how the finished app will look, but with no moving parts or actual code highlighting.

---
+++ pygmentizr.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="")
+app.config.from_object('config')
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
