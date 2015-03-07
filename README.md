8d8a1a33e19e3800f62a537f2f0904eeff10d04b
Alex Chan <alex@alexwlchan.net>
2015-03-07 09:09:48 +0000

    general tidy up and consolidation of code
    All the Python has been moved into a single file `pygmentizr.py` since this is only a small script.
    
    The CSS is now generated on the fly depending on the theme (which can be selected in `pygmentizr.py`.
    
    Minor tweaks to the HTML to make it look slightly better on small screens.

--- README.md
+++ README.md
@@ -14,22 +14,24 @@ Clone or download this repository, then install the requirements with pip:
 
 ## Configuration
 
-The list of lexers/language choices is specified in `config.py`. Here's an example:
+The list of lexers/language choices is specified in `pygmentizr.py`. Here's an example:
 
 ```python
 from pygments.lexers import *
 
 SELECTED_LEXERS = [
-    (PythonLexer, 'Python'),
-    (CoffeeScriptLexer, 'CoffeeScript'),
-    (CssLexer, 'CSS'),
-    (PythonConsoleLexer, 'Python console')
+    ('PythonLexer', 'Python'),
+    ('CoffeeScriptLexer', 'CoffeeScript'),
+    ('CssLexer', 'CSS'),
+    ('PythonConsoleLexer', 'Python console')
 ]
 ```
 
 For each language you want to include, add a tuple to `SELECTED_LEXERS`. The first item should be the Lexer, and the second the name of the language to appear in the list of available languages.
 
 The lexers don't have to be part of the Pygments library. Any Pygments-style lexer can be used here, as long as it's been appropriately imported first.
+
+You can also use any theme which you've imported: just change the value of `PYGMENTS_STYLE` to the name of the style as a string.
 
 ## Running the app
 
--- pygmentizr.py
+++ pygmentizr.py
@@ -3,10 +3,57 @@ from flask.ext.wtf import Form
 from wtforms import SelectField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired
 
-import pygment_utils
+#------------------------------------------------------------------------------
+# CONFIGURATION
+#
+# Define the list of lexers you want to use.
+#
+# Lexers should be entered as a tuple in SELECTED_LEXERS, with the first
+# entry being the Lexer class, and the second entry the name of the language
+# you want to show up in the language chooser. Languages will appear in this
+# order in the form.
+#------------------------------------------------------------------------------
+import pygments
+from pygments.lexers import *
+from pygments.formatters import HtmlFormatter
 
-app = Flask(__name__)
-app.config.from_object('config')
+SELECTED_LEXERS = [
+    ('PythonLexer', 'Python'),
+    ('CoffeeScriptLexer', 'CoffeeScript'),
+    ('CssLexer', 'CSS'),
+    ('PythonConsoleLexer', 'Python console')
+]
+
+# Select the style used by Pygments
+PYGMENTS_STYLE = 'default'
+
+#------------------------------------------------------------------------------
+# Set up the Flask app
+#------------------------------------------------------------------------------
+app = Flask(__name__, template_folder='', static_url_path='', static_folder='')
+
+app.config['WTF_CSRF_ENABLED'] = True
+app.config['SECRET_KEY'] = 'how-not-to-be-seen'
+
+#------------------------------------------------------------------------------
+# Utility functions for rendering code
+#------------------------------------------------------------------------------
+def render_code(code, lexer_name, inline=False):
+    """Takes a string of code and the name of a lexer, and returns a rendered
+    HTML string.
+
+    If inline is true, it uses inline styles to render the text rather than
+    CSS classes. This is useful for environments where external stylesheets are
+    unavailable (e.g. email).
+    """
+    # The lexer name comes from the choices from the SelectField in SnippetForm
+    # so can probably be trusted - even so, we have to be a little careful here.
+    lexer = globals()[lexer_name]
+
+    return pygments.highlight(code,
+                              lexer(),
+                              HtmlFormatter(noclasses=inline,
+                                            style=PYGMENTS_STYLE))
 
 #------------------------------------------------------------------------------
 # Form for handling code snippets
@@ -14,8 +61,7 @@ app.config.from_object('config')
 class SnippetForm(Form):
     language = SelectField('language',
                            validators=[DataRequired()],
-                           choices=pygment_utils.form_choices(),
-                           coerce=int)
+                           choices=SELECTED_LEXERS)
     code = TextAreaField('code_snippet', validators=[DataRequired()])
     inline = BooleanField()
 
@@ -24,12 +70,17 @@ def index():
     form = SnippetForm()
 
     if form.validate_on_submit():
-        html_output = pygment_utils.render_code(form.code.data,
-                                                form.language.data,
-                                                form.inline.data)
+        html_output = render_code(form.code.data,
+                                  form.language.data,
+                                  form.inline.data)
+
+        # Get the CSS used by this style to include on the page
+        css = HtmlFormatter(style=PYGMENTS_STYLE).get_style_defs()
+
         return render_template("index.html",
                                form=form,
-                               html_output=html_output)
+                               html_output=html_output,
+                               css=css)
 
     return render_template("index.html",
                            form=form)
